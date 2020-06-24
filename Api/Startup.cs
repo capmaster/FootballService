7612af06004fd533f78fc9d877a299007b53558a
rakshith.shetty <rakshith.shetty@HIRING.PUBLICISSAPIENT.COM>
2020-06-24 16:49:31 +0530

    updated service regn

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -28,6 +28,7 @@ namespace Api
         {
             services.AddControllers();
             services.AddScoped<IFootballService,FootballService>();
+            services.AddScoped<IMathService,MathService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
